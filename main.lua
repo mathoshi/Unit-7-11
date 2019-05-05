217b714fc0fa08c50ba1c1eed8f1d2ad387c5b44
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-05-05 19:49:54 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@
 local physics = require( "physics")
 
 physics.start()
-physics.setGravity( 0, 10)
+physics.setGravity( 0, 20)
 physics.setDrawMode("hybrid")
 
 local playerBullets = {} -- Table that holds the players Bullets
@@ -50,7 +50,7 @@ local sheetOptionsAttack =
 {
     width = 536,
     height = 495,
-    numFrames = 10 
+    numFrames = 12 
 }
 local sheetAttackingNinja = graphics.newImageSheet( "assets/NinjaAttack.png", sheetOptionsAttack )
 
@@ -70,7 +70,7 @@ local sequence_data = {
         start = 1,
         count = 10,
         time = 800,
-        loopCount = 0,
+        loopCount = 1,
         sheet = sheetDeadNinja
     }
 }
@@ -95,7 +95,7 @@ local sequenceData = {
 }
 
 local AttackNinja = display.newSprite( sheetAttackingNinja, sequenceData )
-AttackNinja.x = 50
+AttackNinja.x = 90
 AttackNinja.y = 400
 AttackNinja.xScale = 100/536
 AttackNinja.yScale = 92/495
@@ -110,12 +110,13 @@ AttackNinja:setSequence( "idle")
 AttackNinja:play()
 
 local ninja = display.newSprite( sheetIdleNinja, sequence_data )
-ninja.x = display.contentWidth / 2
+ninja.x = 250
 ninja.y = 300
 ninja.xScale = 58/363
 ninja.yScale = 77/458
 ninja.width = 58
 ninja.height = 77
+ninja.id = "normal ninja"
 physics.addBody( ninja, "dynamic", {
     fricition = 0.5,
     bounce = 0.1
@@ -132,24 +133,75 @@ ninja:play()
 --
 --timer.performWithDelay( 2000, swapSheet )
 
+local function shootBullet()
+    -- make a bullet appear
+    local aSingleBullet = display.newCircle( 0, 0, 10 )
+    aSingleBullet.x = AttackNinja.x +10
+    aSingleBullet.y = AttackNinja.y
+    physics.addBody( aSingleBullet, 'dynamic' )
+    -- Make the object a "bullet" type object
+    aSingleBullet.isBullet = true
+    aSingleBullet.gravityScale = (10)
+    aSingleBullet.id = "bullet"
+    aSingleBullet:setLinearVelocity( 700, 0 )
+
+    table.insert(playerBullets,aSingleBullet)
+    print("# of bullet: " .. tostring(#playerBullets))
+end
+
+local function goIdle()
+    AttackNinja:setSequence( "idle")
+    AttackNinja:play()
+end
+
 function shootButton:touch( event )
     if ( event.phase == "began" ) then
-            AttackNinja:setSequence( "attack")
-            AttackNinja:play()
-            -- make a bullet appear
-            local aSingleBullet = display.newCircle( 0, 0, 10 )
-            aSingleBullet.x = AttackNinja.x +10
-            aSingleBullet.y = AttackNinja.y
-            physics.addBody( aSingleBullet, 'dynamic' )
-            -- Make the object a "bullet" type object
-            aSingleBullet.isBullet = true
-            aSingleBullet.gravityScale = (10)
-            aSingleBullet.id = "bullet"
-            aSingleBullet:setLinearVelocity( 1000, 0 )
-
-            table.insert(playerBullets,aSingleBullet)
-            print("# of bullet: " .. tostring(#playerBullets))
+        AttackNinja:setSequence( "attack")
+        AttackNinja:play()
+        timer.performWithDelay( 200, shootBullet )
+        timer.performWithDelay( 700, goIdle)        
     end
 end
+
+local function onCollision( event )
+ 
+   if ( event.phase == "began" ) then
+
+       local obj1 = event.object1
+       local obj2 = event.object2
+       local whereCollisonOccurredX = obj1.x
+       local whereCollisonOccurredY = obj1.y
+
+       if ( ( obj1.id == "normal ninja" and obj2.id == "bullet" ) or
+            ( obj1.id == "bullet" and obj2.id == "normal ninja" ) ) then
+           -- Remove both the laser and asteroid
+           --display.remove( obj1 )
+           --display.remove( obj2 )
+           
+           -- remove the bullet
+           local bulletCounter = nil
+           
+           for bulletCounter = #playerBullets, 1, -1 do
+               if ( playerBullets[bulletCounter] == obj1 or playerBullets[bulletCounter] == obj2 ) then
+                   playerBullets[bulletCounter]:removeSelf()
+                   playerBullets[bulletCounter] = nil
+                   table.remove( playerBullets, bulletCounter )
+                   break
+               end
+           end
+
+           --remove character
+           display.remove(aSingleBullet)
+
+           ninja:setSequence("dead")
+           ninja:play()
+
+
+       end
+   end
+end
+
+        Runtime:addEventListener( "collision", onCollision )
+
 
 shootButton:addEventListener( "touch", shootButton)
